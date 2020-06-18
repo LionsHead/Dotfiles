6d34609d53e4fe0a3d2bb6bd31187479cdf8faa0
Alexandr Savin <aleksandr.vl.savin@rt.ru>
2020-06-18 15:34:57 +0400

    update vscode settings

--- vscode/extensions.sh
+++ vscode/extensions.sh
@@ -2,21 +2,16 @@
 #
 # code --list-extensions | xargs -L 1 echo code --install-extension
 #
-
-code --install-extension aaron-bond.better-comments
-code --install-extension akamud.vscode-theme-onelight
+                                                                                                                                                             15:32:34
 code --install-extension BriteSnow.vscode-toggle-quotes
 code --install-extension eamodio.gitlens
-code --install-extension emmanuelbeziat.vscode-great-icons
 code --install-extension file-icons.file-icons
 code --install-extension formulahendry.terminal
 code --install-extension golang.Go
 code --install-extension groksrc.ruby
 code --install-extension hoovercj.ruby-linter
-code --install-extension Hyzeta.vscode-theme-github-light
 code --install-extension JakeBecker.elixir-ls
 code --install-extension jduponchelle.rainbow-end
-code --install-extension JonaDuran.my-light-theme
 code --install-extension kaiwood.endwise
 code --install-extension misogi.ruby-rubocop
 code --install-extension ms-azuretools.vscode-docker
@@ -26,7 +21,6 @@ code --install-extension rebornix.ruby
 code --install-extension rust-lang.rust
 code --install-extension Shan.code-settings-sync
 code --install-extension TabNine.tabnine-vscode
-code --install-extension thenikso.github-plus-theme
 code --install-extension tonsky.theme-alabaster
 code --install-extension wingrunr21.vscode-ruby
 code --install-extension yzhang.markdown-all-in-one
