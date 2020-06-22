0e551578a3f2ec651fabe88eb312db423bd10773
Alexandr Savin <aleksandr.vl.savin@rt.ru>
2020-06-22 16:43:04 +0400

    upd vscode config

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
+#
+# code --list-extensions | xargs -L 1 echo code --install-extension
+#
 
-rm -rf ~/Library/Application\ Support/Code/User
-ln -s "${DOTFILES_PATH}vscode/User" "~/Library/Application\ Support/Code/User"
 
+if ![ -x "$(command -v code)" ]; then
 export PATH="\$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+fi
 
-source vscode/extensions.sh
+code --install-extension BriteSnow.vscode-toggle-quotes
+code --install-extension eamodio.gitlens
+code --install-extension file-icons.file-icons
+code --install-extension formulahendry.terminal
+code --install-extension golang.Go
+code --install-extension groksrc.ruby
+code --install-extension hoovercj.ruby-linter
+code --install-extension JakeBecker.elixir-ls
+code --install-extension jduponchelle.rainbow-end
+code --install-extension kaiwood.endwise
+code --install-extension misogi.ruby-rubocop
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-vscode.atom-keybindings
+code --install-extension pgourlain.erlang
+code --install-extension rebornix.ruby
+code --install-extension rust-lang.rust
+code --install-extension Shan.code-settings-sync
+code --install-extension TabNine.tabnine-vscode
+code --install-extension tonsky.theme-alabaster
+code --install-extension wingrunr21.vscode-ruby
+code --install-extension yzhang.markdown-all-in-one
+code --install-extension zerokol.vscode-eex-beautify
