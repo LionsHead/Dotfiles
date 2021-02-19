0c74962e7a1ab214499bedf6c5cc66a7a1f0835a
Alexandr Savin <aleksandr.vl.savin@rt.ru>
2021-02-19 12:29:44 +0400

    fix installer

--- install.sh
+++ install.sh
@@ -33,48 +33,48 @@ read -r -p "Install asdf tools? [Y/n] " response_asdf_tools
 read -r -p "Use default settings for iTerm2? [y/n] " response_iterm
 read -r -p "Install vscode extensions? [y/N] " response_vscode
 
-# if [[ $response_backup =~ (y|yes|Y) ]]; then
-#   mkdir -p ~/.old_dotfiles
-#   find ~/.** -type f -maxdepth 0 -exec cp {}  ~/.old_dotfiles/ \;
+if [[ $response_backup =~ (y|yes|Y) ]]; then
+  mkdir -p ~/.old_dotfiles
+  find ~/.** -type f -maxdepth 0 -exec cp {}  ~/.old_dotfiles/ \;
 
-#   cp ~/.ssh/config ~/.old_dotfiles/
-# fi
+  cp ~/.ssh/config ~/.old_dotfiles/
+fi
 
 # # OS X defaults
-# if [[ $response_defaults =~ (y|yes|Y) ]]; then
-#   source osx/defaults.sh
+if [[ $response_defaults =~ (y|yes|Y) ]]; then
+  source osx/defaults.sh
 
-#   success 'Mac os configured'
-# fi
+  success 'Mac os configured'
+fi
 
-# Other dotfiles
+## Other dotfiles
 ## SSH
-# ln -fisv "${DOTFILES_PATH}ssh/config" ~/.ssh/config
-# link ".editorconfig"
-# ## elixir
-# link "elixir/.iex.exs"
-# ## ruby
-# link "ruby/.gemrc"
-# link "ruby/.default-gems"
+ln -fisv "${DOTFILES_PATH}ssh/config" ~/.ssh/config
+link ".editorconfig"
+## elixir
+link "elixir/.iex.exs"
+## ruby
+link "ruby/.gemrc"
+link "ruby/.default-gems"
 
 # Setup git config
 source git/install.sh
-# # Install brew
-# source brew/install.sh
-# # ASDF version manager
-# source asdf/install.sh
+# Install brew
+source brew/install.sh
+# ASDF version manager
+source asdf/install.sh
 
-# # iTerm2
-# if [[ $response_iterm =~ (y|yes|Y) ]]; then
-#   source iterm2/install.sh
-# fi
+# iTerm2
+if [[ $response_iterm =~ (y|yes|Y) ]]; then
+  source iterm2/install.sh
+fi
 
-# # Visual Studio Code
-# if [[ $response_vscode =~ (y|yes|Y) ]]; then
-#   source vscode/install.sh
-# fi
+# Visual Studio Code
+if [[ $response_vscode =~ (y|yes|Y) ]]; then
+  source vscode/install.sh
+fi
 
-# # ZSH
-# source zsh/install.sh
+# ZSH
+source zsh/install.sh
 
 success 'Done.'
