11ce0c91579d5dc163748d89f86d1acbd7c74158
Aleksandr Savin <lionshorse@gmail.com>
2023-04-25 16:34:07 +0400

    use apple keychain

--- ssh/install.sh
+++ ssh/install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+info () {
+  printf "\r  [ \033[00;34m..\033[0m ] $1\n"
+}
+
 info "Installing ssh keys"
 
-link "ssh/config"
+# load ssh keys by keychain
+ssh-add --apple-load-keychain  ~/.ssh/id_rsa
+
+# ssh
+ln -fisv "${DOTFILES_PATH}ssh/config" ~/.ssh/config
